1fa77b6569c6a8990ed3574ffa550dcd9add9860
Aaron Sanchez <saarong12@gmail.com>
2024-02-21 23:57:58 -0600

    Added url to get season by ID as well as get the api_token from the .env file

--- server.ts
+++ server.ts
@@ -1,7 +1,16 @@
 import express, { Express, Request, Response} from 'express'
+import 'dotenv/config'
+
 const port = 8080
 
 const app: Express = express()
+
+const seasonIDs = {
+  champsLeague: 21638,
+  mls: 22974
+}
+
+let seasonsBaseUrl: string = 'https://api.sportmonks.com/v3/football/seasons/21638'
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello')  
