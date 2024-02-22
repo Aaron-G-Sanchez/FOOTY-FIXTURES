d73c0a14ee67609913ad0b7adcdb89ef7aa44366
Aaron Sanchez <saarong12@gmail.com>
2024-02-22 00:30:44 -0600

    Added ability to specify Champions League or MLS games

--- server.ts
+++ server.ts
@@ -7,15 +7,18 @@ const port = 8080
 const app: Express = express()
 
 const seasonIDs = {
-  champsLeague: 21638,
-  mls: 22974
+  ucl: 21638, // UEFA Champions League
+  mls: 22974 // Major League Soccer
 }
 
-const seasonsBaseUrl: string = 'https://api.sportmonks.com/v3/football/seasons/21638'
+const seasonsBaseUrl: string = 'https://api.sportmonks.com/v3/football/seasons'
 
-app.get('/', async (req: Request, res: Response) => {
-  const response = await fetch(`${seasonsBaseUrl}?api_token=${process.env.API_TOKEN}&include=fixtures`)
+app.get('/:league', async (req: Request, res: Response) => {
+  const { league } = req.params
+  
+  const response = await fetch(`${seasonsBaseUrl}/${seasonIDs[league]}?api_token=${process.env.API_TOKEN}&include=fixtures`)
   const data = await response.json()
+
   console.log(JSON.stringify(data, null , 2));
 
   res.send(data)  
