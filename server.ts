58abb117e8091894aa7b9adc1313d721c18bb277
Aaron Sanchez <saarong12@gmail.com>
2024-02-22 00:23:19 -0600

    Created first call to get season by ID and include the fixtures

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response} from 'express'
+
 import 'dotenv/config'
 
 const port = 8080
@@ -10,10 +11,14 @@ const seasonIDs = {
   mls: 22974
 }
 
-let seasonsBaseUrl: string = 'https://api.sportmonks.com/v3/football/seasons/21638'
+const seasonsBaseUrl: string = 'https://api.sportmonks.com/v3/football/seasons/21638'
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello')  
+app.get('/', async (req: Request, res: Response) => {
+  const response = await fetch(`${seasonsBaseUrl}?api_token=${process.env.API_TOKEN}&include=fixtures`)
+  const data = await response.json()
+  console.log(JSON.stringify(data, null , 2));
+
+  res.send(data)  
 })
 
 app.listen(port, () => {
